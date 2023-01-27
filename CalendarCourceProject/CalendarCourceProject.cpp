ffc2e53c1d1195e67534ed00e05095330430024e
NadezhdaSavcheva <60537275+NadezhdaSavcheva@users.noreply.github.com>
2023-01-27 12:40:53 +0200

    Final commit.

--- CalendarCourceProject/CalendarCourceProject.cpp
+++ CalendarCourceProject/CalendarCourceProject.cpp
@@ -441,22 +441,6 @@ void calendarStartMenu()
 }
 
 /// <summary>
-/// Function that calculates the number of days in a given month and year.
-/// </summary>
-/// <param name="day"></param>
-/// <param name="month"></param>
-/// <param name="year"></param>
-/// <returns>number of days</returns>
-//int dayNumber(int day, int month, int year)
-//{
-//    static int t[] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
-//
-//    year -= month < 3;
-//
-//    return (year + year / 4 - year / 100 + year / 400 + t[month - 1] + day) % 7;
-//}
-
-/// <summary>
 /// Function that returns the name of a month.
 /// </summary>
 /// <param name="monthNumber"></param>
