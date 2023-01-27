175fe90f21ecdc4198554b50f0964fa6bce39f09
NadezhdaSavcheva <60537275+NadezhdaSavcheva@users.noreply.github.com>
2023-01-27 12:38:20 +0200

    Removing unnecessary function.

--- CalendarCourceProject/CalendarCourceProject.cpp
+++ CalendarCourceProject/CalendarCourceProject.cpp
@@ -447,14 +447,14 @@ void calendarStartMenu()
 /// <param name="month"></param>
 /// <param name="year"></param>
 /// <returns>number of days</returns>
-int dayNumber(int day, int month, int year)
-{
-    static int t[] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
-
-    year -= month < 3;
-
-    return (year + year / 4 - year / 100 + year / 400 + t[month - 1] + day) % 7;
-}
+//int dayNumber(int day, int month, int year)
+//{
+//    static int t[] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
+//
+//    year -= month < 3;
+//
+//    return (year + year / 4 - year / 100 + year / 400 + t[month - 1] + day) % 7;
+//}
 
 /// <summary>
 /// Function that returns the name of a month.
